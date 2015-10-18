fa05fccf30ccb7372baf6f2472e7ba60bbf92d44
sashok724 <mail@sashok724.net>
2015-10-18 14:31:50 +0400

    Добавлен LaunchServer API скрипт и пример plugin.js

---
+++ LaunchServer/runtime/apiServer.js
@@ -0,0 +1,19 @@
+server.loadScript(IOHelperClass.static.toURL(java.nio.file.Paths.get("api.js")));
+var LaunchServer = LaunchServerClass.static;
+
+// Auth class API imports
+var AuthHandler = AuthHandlerClass.static;
+var FileAuthHandler = FileAuthHandlerClass.static;
+var CachedAuthHandler = CachedAuthHandlerClass.static;
+var AuthProvider = AuthProviderClass.static;
+var DigestAuthProvider = DigestAuthProviderClass.static;
+var AuthException = AuthExceptionClass.static;
+
+// Command class API imports
+var Command = CommandClass.static;
+var CommandHandler = CommandHandlerClass.static;
+var CommandException = CommandExceptionClass.static;
+
+// Response class API imports
+var Response = ResponseClass.static;
+var ResponseFactory = ResponseFactoryClass.static;
---
+++ LaunchServer/runtime/plugin.js
@@ -0,0 +1,4 @@
+server.loadScript(IOHelperClass.static.toURL(java.nio.file.Paths.get("apiServer.js")));
+
+// Print test message
+LogHelper.info("This message was printed from plugin.js");

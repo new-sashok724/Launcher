2c733b40355f83d132cc1490fe7434ee8083d9d2
sashok724 <mail@sashok724.net>
2015-12-26 20:53:05 +0400

    Код оверлеев

--- Launcher/runtime/source/dialog/DialogController.java
+++ Launcher/runtime/source/dialog/DialogController.java
@@ -5,20 +5,24 @@ import java.net.URL;
 import java.nio.file.NoSuchFileException;
 import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.ButtonBase;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBoxBase;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import launcher.client.ClientProfile;
 import launcher.helper.IOHelper;
 import launcher.helper.LogHelper;
@@ -34,7 +38,7 @@ public final class DialogController {
 	private final Stage stage;
 
 	// Layout
-	@FXML private Parent root, layout, overlayDim;
+	@FXML private Pane root, layout, overlayDim, overlay;
 	@FXML private WebView news;
 
 	// Input controls
@@ -49,6 +53,26 @@ public final class DialogController {
 	public DialogController(Application app, Stage stage) {
 		this.app = app;
 		this.stage = stage;
+	}
+
+	public void hideOverlay(double delay, EventHandler<ActionEvent> handler) {
+		fade(overlay, delay, 1.0D, 0.0D, e -> {
+			overlay.setVisible(false);
+			overlayDim.getChildren().remove(overlay);
+			fade(overlayDim, 0.0D, 1.0D, 0.0D, e2 -> {
+				overlayDim.setVisible(false);
+
+				// Unfreeze layout
+				layout.setDisable(false);
+				layout.requestFocus();
+
+				// Reset overlay state
+				overlay = null;
+				if (handler != null) {
+					handler.handle(e2);
+				}
+			});
+		});
 	}
 
 	public void initOfflineMode() throws NoSuchFileException {
@@ -70,7 +94,7 @@ public final class DialogController {
 		news.getEngine().load(offlineURL.toString());
 	}
 
-	public Parent loadDialog() throws IOException {
+	public Pane loadDialog() throws IOException {
 		loadFXML(IOHelper.getResourceURL("launcher/runtime/dialog/dialog.fxml"), this);
 
 		// Initialize webview
@@ -107,6 +131,54 @@ public final class DialogController {
 
 		// Return initialized root
 		return root;
+	}
+
+	public void showOverlay(Pane newOverlay, EventHandler<ActionEvent> handler) {
+		layout.setDisable(true);
+		overlay = newOverlay;
+
+		// Show dim pane
+		overlayDim.setVisible(true);
+		overlayDim.toFront();
+		fade(overlayDim, 0.0D, 0.0D, 1.0D, e -> {
+			overlayDim.requestFocus();
+			overlayDim.getChildren().add(newOverlay);
+
+			// Show overlay
+			newOverlay.setVisible(true);
+			fade(newOverlay, 0.0D, 0.0D, 1.0D, handler);
+		});
+	}
+
+	public void swapOverlay(double delay, Pane newOverlay, EventHandler<ActionEvent> handler) {
+		fade(overlay, delay, 1.0D, 0.0D, e -> {
+			overlay.setVisible(false);
+			overlayDim.requestFocus();
+
+			// Swap overlays
+			if (!newOverlay.equals(overlay)) {
+				ObservableList<Node> children = overlayDim.getChildren();
+				children.set(children.indexOf(overlay), newOverlay);
+				overlay = newOverlay;
+			}
+
+			// Show new overlay
+			overlay.setVisible(true);
+			fade(overlay, 0.0D, 0.0D, 1.0D, handler);
+		});
+	}
+
+	private void fade(Node node, double delay, double from, double to, EventHandler<ActionEvent> handler) {
+		FadeTransition transition = new FadeTransition(Duration.millis(100.0D), node);
+		if (handler != null) {
+			transition.setOnFinished(handler);
+		}
+
+		// Launch transition
+		transition.setDelay(javafx.util.Duration.millis(delay));
+		transition.setFromValue(from);
+		transition.setToValue(to);
+		transition.play();
 	}
 
 	private void play(ActionEvent e) {

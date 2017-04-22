abbfe3eaf289379cef5bf2388561556a71fbafbc
sashok724 <mail@sashok724.net>
2017-04-22 18:31:58 +0400

    Патч банжи

--- Launcher/source-authlib/yggdrasil/LegacyBridge.java
+++ Launcher/source-authlib/yggdrasil/LegacyBridge.java
@@ -1,12 +1,19 @@
 package com.mojang.authlib.yggdrasil;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import launcher.LauncherAPI;
 import launcher.client.ClientLauncher;
+import launcher.client.PlayerProfile;
 import launcher.helper.CommonHelper;
 import launcher.helper.IOHelper;
 import launcher.helper.LogHelper;
+import launcher.helper.SecurityHelper;
 import launcher.request.auth.CheckServerRequest;
 import launcher.request.auth.JoinServerRequest;
 
+@LauncherAPI
 public final class LegacyBridge {
     private LegacyBridge() {
     }
@@ -15,6 +22,30 @@ public final class LegacyBridge {
     public static boolean checkServer(String username, String serverID) throws Exception {
         LogHelper.debug("LegacyBridge.checkServer, Username: '%s', Server ID: %s", username, serverID);
         return new CheckServerRequest(username, serverID).request() != null;
+    }
+
+    @SuppressWarnings("unused") // Result is user properties (Used by BungeeCord)
+    public static Map<String, String> checkServerWithProperties(String username, String serverID) throws Exception {
+        PlayerProfile pp = new CheckServerRequest(username, serverID).request();
+        if (pp == null) {
+            return null;
+        }
+
+        // Add properties
+        Map<String, String> properties = new HashMap<>(5);
+        properties.put("uuid", pp.uuid.toString());
+        properties.put("uuid-hash", ClientLauncher.toHash(pp.uuid));
+        if (pp.skin != null) {
+            properties.put(ClientLauncher.SKIN_URL_PROPERTY, pp.skin.url);
+            properties.put(ClientLauncher.SKIN_DIGEST_PROPERTY, SecurityHelper.toHex(pp.skin.digest));
+        }
+        if (pp.cloak != null) {
+            properties.put(ClientLauncher.SKIN_URL_PROPERTY, pp.cloak.url);
+            properties.put(ClientLauncher.SKIN_DIGEST_PROPERTY, SecurityHelper.toHex(pp.cloak.digest));
+        }
+
+        // We're done
+        return properties;
     }
 
     @SuppressWarnings("unused")

f4822401c0c3bf8961c4fc2cb0535d261e326ad9
sashok724 <mail@sashok724.net>
2017-08-21 00:38:04 +0400

    Нехватающее

--- Launcher/source-authlib/yggdrasil/CompatProfile.java
+++ Launcher/source-authlib/yggdrasil/CompatProfile.java
@@ -2,11 +2,19 @@ package com.mojang.authlib.yggdrasil;
 
 import java.util.UUID;
 
+import launcher.LauncherAPI;
 import launcher.client.ClientLauncher;
 import launcher.client.PlayerProfile;
 import launcher.helper.SecurityHelper;
 
+@LauncherAPI
 public final class CompatProfile {
+    public static final String SKIN_URL_PROPERTY = ClientLauncher.SKIN_URL_PROPERTY;
+    public static final String SKIN_DIGEST_PROPERTY = ClientLauncher.SKIN_DIGEST_PROPERTY;
+    public static final String CLOAK_URL_PROPERTY = ClientLauncher.CLOAK_URL_PROPERTY;
+    public static final String CLOAK_DIGEST_PROPERTY = ClientLauncher.CLOAK_DIGEST_PROPERTY;
+
+    // Instance
     public final UUID uuid;
     public final String uuidHash, username;
     public final String skinURL, skinHash;
@@ -14,12 +22,29 @@ public final class CompatProfile {
 
     public CompatProfile(UUID uuid, String username, String skinURL, String skinHash, String cloakURL, String cloakHash) {
         this.uuid = uuid;
-        this.uuidHash = ClientLauncher.toHash(uuid);
+        uuidHash = ClientLauncher.toHash(uuid);
         this.username = username;
         this.skinURL = skinURL;
         this.skinHash = skinHash;
         this.cloakURL = cloakURL;
         this.cloakHash = cloakHash;
+    }
+
+    public int countProperties() {
+        int count = 0;
+        if (skinURL != null) {
+            count++;
+        }
+        if (skinHash != null) {
+            count++;
+        }
+        if (cloakURL != null) {
+            count++;
+        }
+        if (cloakHash != null) {
+            count++;
+        }
+        return count;
     }
 
     public static CompatProfile fromPlayerProfile(PlayerProfile profile) {
